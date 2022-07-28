d1c1c9c324bb070b79d67ad1d64f1d36ed12c62b
jimiait <106033479+jimiait@users.noreply.github.com>
2022-07-28 12:21:29 -0500

    EXLProject1

--- src/main/java/page/BasePage.java
+++ src/main/java/page/BasePage.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class BasePage {
 	
@@ -16,6 +17,10 @@ public class BasePage {
 	public void selectFromDropdown(WebElement element, String visibleText) {
 		Select sel = new Select(element);
 		sel.selectByVisibleText(visibleText);
+	}
+	
+	public void pageVerification(String actual, String expected, String message) {
+		Assert.assertEquals(actual, expected, message);
 	}
 
 }
--- src/main/java/page/ContactsPage.java
+++ src/main/java/page/ContactsPage.java
@@ -1,12 +1,9 @@
 package page;
 
-import java.util.Random;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class ContactsPage extends BasePage{
